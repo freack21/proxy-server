4ccbe28141fc88ad773029fcee9d49e05f1d9c81
freack21 <fixri2104@gmail.com>
2024-12-25 00:49:08 +0700

    fix: config router

--- route/config.js
+++ route/config.js
@@ -1,4 +1,9 @@
-const { queryToJSON, loadConfig, setConfigValue, sendJSON } = require("../util");
+const {
+  queryToJSON,
+  loadConfig,
+  setConfigValue,
+  sendJSON,
+} = require("../util");
 
 const config = (req, res) => {
   let CONFIG = loadConfig();
@@ -6,36 +11,22 @@ const config = (req, res) => {
   if (req.method === "GET") {
     const query = req.url.split("?")[1] || "";
     const data = queryToJSON(query);
-    if (data.key == process.env.SECRET_KEY) {
-      return sendJSON(res, 200, CONFIG);
-    }
-    return sendJSON(res, 401, { msg: "Unauthorized" });
-  } else if (req.method === "POST") {
-    const query = req.url.split("?")[1] || "";
-    const data = queryToJSON(query);
     if (data.key != process.env.SECRET_KEY) {
       return sendJSON(res, 401, { msg: "Unauthorized" });
     }
+    if (!data.act || data.act == "get") return sendJSON(res, 200, CONFIG);
+    else if (data.act == "set") {
+      delete data.key;
+      delete data.act;
 
-    let body = "";
-
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-
-    req.on("end", () => {
-      try {
-        const data = JSON.parse(body);
-
-        for (const key in data) {
-          setConfigValue(key, data[key]);
-        }
-
-        return sendJSON(res, 200, CONFIG);
-      } catch (e) {
-        return sendJSON(res, 400, { msg: "Invalid JSON" });
+      for (const key in data) {
+        if (isNaN(Number(data[key])) || data[key] < 0) continue;
+        setConfigValue(key, data[key]);
       }
-    });
+
+      return sendJSON(res, 200, CONFIG);
+    }
+  } else {
   }
 };
 
