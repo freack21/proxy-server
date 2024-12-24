c21a6b13df673b66b7f1f994f88f497aaf78cd13
freack21 <fixri2104@gmail.com>
2024-12-24 23:08:43 +0700

    feat: sendJSON and redirect

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const https = require("https");
 const http = require("http");
 const fs = require("fs");
 const httpProxy = require("http-proxy");
+const {
+  getPortForSubdomain,
+  parseDomain,
+  sendJSON,
+  redirect,
+} = require("./util");
 
 const sslOptions = {
   key: fs.readFileSync(
@@ -18,56 +24,27 @@ const sslOptions = {
 
 const proxy = httpProxy.createProxyServer({});
 
-function getPortForSubdomain(subdomain, firstDomain) {
-  const myFirstDomain = process.env.DOMAIN.split(".")[0];
-  if (firstDomain == myFirstDomain) {
-    return 0;
-  }
-
-  switch (subdomain) {
-    case "moviepass":
-      return 3200;
-    case "www":
-      return 0;
-    default:
-      return -1;
-  }
-}
-
 const server = https.createServer(sslOptions, (req, res) => {
-  const myDomain = process.env.DOMAIN.split(".").join("\\.");
-  const regex = new RegExp(`^([a-zA-Z0-9-]+)\\.${myDomain}$`);
-
   const host = req.headers.host || "";
-  const match = host.match(regex);
-
-  const subdomain = match ? match[1] : "";
-  const firstDomain = host.split(".")[0];
-
-  const port = getPortForSubdomain(subdomain, firstDomain);
+  const subdomain = parseDomain(host);
+  const port = getPortForSubdomain(subdomain);
 
   if (port === -1) {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end({ msg: "Not Found" });
-    return;
+    return sendJSON(res, 404, { msg: "Not Found" });
   } else if (port === 0) {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end({ msg: "Hello World" });
-    return;
+    return sendJSON(res, 200, { msg: "Hello World" });
   }
 
   proxy.web(req, res, { target: `http://127.0.0.1:${port}` });
 });
 
 server.listen(443, () => {
-  console.log("Proxy server running on https://" + process.env.DOMAIN);
+  console.log("Proxy Server running on https://" + process.env.DOMAIN);
 });
 
 http
   .createServer((req, res) => {
-    const host = req.headers.host;
-    res.writeHead(301, { Location: `https://${host}${req.url}` });
-    res.end();
+    redirect(req, res);
   })
   .listen(80, () => {
     console.log("HTTP Redirect Server running!");
---
+++ util.js
@@ -0,0 +1,31 @@
+const config = require("./config");
+
+const parseDomain = (domain) => {
+  const regex = /^(?:(\w+)\.)?(\w+)\.(\w+)$/;
+  const match = domain.match(regex) || [];
+
+  return match[1] || match[2] || "";
+};
+
+const getPortForSubdomain = (subdomain) => {
+  return config[subdomain] || -1;
+};
+
+const sendJSON = (res, statusCode, data) => {
+  res.writeHead(statusCode, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(data));
+};
+
+const redirect = (req, res) => {
+  const host = req.headers.host;
+  location = `https://${host}${req.url}`;
+  res.writeHead(301, { Location: location });
+  res.end();
+};
+
+module.exports = {
+  parseDomain,
+  getPortForSubdomain,
+  sendJSON,
+  redirect,
+};
